fc0cf7cd21d16e5af7643ae80644ae0ab462db12
Barkov Evgeny <barkovevgenii@inbox.ru>
2018-08-26 20:44:33 +0300

    Task based testbench has been added

--- tb/tb_light_manager.v
+++ tb/tb_light_manager.v
@@ -36,7 +36,11 @@ reg           clk, rst_n;
 wire  [3 : 0] future_leds;
 
 // GPIO interface signals
-reg           a_i, b_i;
+wire           a_i, b_i;
+
+// ( dir == 1 ) ? ( right rotate ) : ( left rotate )
+reg             dir;
+reg             a, b;
   
 light_manager #(
   .CLOCK_FREQ_MHZ         ( CLOCK_FREQ_MHZ   ),
@@ -45,7 +49,7 @@ light_manager #(
   .BRIGHTNESS_INC         ( BRIGHTNESS_INC   )
 ) light_manager_0 (
   .clk_i                  ( clk              ),
-  .rst_n_i                  ( rst_n              ),
+  .rst_n_i                ( rst_n            ),
   
   .leds_o                 ( future_leds      ),
   
@@ -54,152 +58,54 @@ light_manager #(
   .b_i                    ( b_i              )
 );
 
-/*
-// my unsuccessful attempts are commented
+assign a_i = ( dir ) ? ( a ) : ( b );
+assign b_i = ( dir ) ? ( b ) : ( a );
 
-integer a, b, c, d;
-
-task rotate_the_shaft(
-  // 0 - left, 1 - right
-  input dir
-);
-  if ( dir )
-    fork
-      // below is equivalent to put_rotate_wave( .wave( a_i ) );
-      begin
-        for ( a = 0; a < CLOCK_FREQ_MHZ * EXPECTED_DELAY_IN_US; a = a + 1 )
-          begin
-            @( posedge clk );
-            a_i = $urandom % 2;
-            $display("YA V 1ST FORE GDE A");
-          end
-        a_i = 0;
-        #( 5 * EXPECTED_DELAY_IN_US * 1000 );
-        for ( a = 0; a < CLOCK_FREQ_MHZ * EXPECTED_DELAY_IN_US; a = a + 1 )
-          begin
-            @( posedge clk );
-            a_i = $urandom % 2;
-            $display("YA V 2ND FORE GDE A");
-          end
-        a_i = 1;
-      end
-      // below is equivalent to #( 3 * EXPECTED_DELAY_IN_US * 1000 ) put_rotate_wave( .wave( b_i ) );
-      #( 3 * EXPECTED_DELAY_IN_US * 1000 ) 
-      begin
-        for ( b = 0; b < CLOCK_FREQ_MHZ * EXPECTED_DELAY_IN_US; b = b + 1 )
-          begin
-            @( posedge clk );
-            b_i = $urandom % 2;
-            $display("YA V 1ST FORE GDE B");
-          end
-        b_i = 0;
-        #( 5 * EXPECTED_DELAY_IN_US * 1000 );
-        for ( b = 0; b < CLOCK_FREQ_MHZ * EXPECTED_DELAY_IN_US; b = b + 1 )
-          begin
-            @( posedge clk );
-            b_i = $urandom % 2;
-            $display("YA V 2ND FORE GDE B");
-          end
-        b_i = 1;
-      end
-    join
-  else
-    fork
-      // below is equivalent to put_rotate_wave( .wave( b_i ) );
-      begin
-        for ( c = 0; c < CLOCK_FREQ_MHZ * EXPECTED_DELAY_IN_US; c = c + 1 )
-          begin
-            @( posedge clk );
-            b_i = $urandom % 2;
-            $display("YA V 1ST FORE GDE C");
-          end
-        b_i = 0;
-        #( 5 * EXPECTED_DELAY_IN_US * 1000 );
-        for ( c = 0; c < CLOCK_FREQ_MHZ * EXPECTED_DELAY_IN_US; c = c + 1 )
-          begin
-            @( posedge clk );
-            b_i = $urandom % 2;
-            $display("YA V 2ND FORE GDE C");
-          end
-        b_i = 1;
-      end
-      // below is equivalent to #( 3 * EXPECTED_DELAY_IN_US * 1000 ) put_rotate_wave( .wave( a_i ) );
-      #( 3 * EXPECTED_DELAY_IN_US * 1000 )
-      begin
-        for ( d = 0; d < CLOCK_FREQ_MHZ * EXPECTED_DELAY_IN_US; d = d + 1 )
-          begin
-            @( posedge clk );
-            a_i = $urandom % 2;
-            $display("YA V 1ST FORE GDE D");
-          end
-        a_i = 0;
-        #( 5 * EXPECTED_DELAY_IN_US * 1000 );
-        for ( d = 0; d < CLOCK_FREQ_MHZ * EXPECTED_DELAY_IN_US; d = d + 1 )
-          begin
-            @( posedge clk );
-            a_i = $urandom % 2;
-            $display("YA V 2ND FORE GDE D");
-          end
-        a_i = 1;
-      end
-    join
+task put_rotate_waves();
+  fork
+    put_rotate_wave_a();
+    #( 3 * EXPECTED_DELAY_IN_US * 1000 ) put_rotate_wave_b();
+    #( 12 * EXPECTED_DELAY_IN_US * 1000 );
+  join
 endtask
 
-/*
-task rotate_the_shaft(
-  // 0 - left, 1 - right
-  input dir
-);
-  if ( dir )
-    fork
-      put_rotate_wave( .wave( a_i ) );
-      #( 3 * EXPECTED_DELAY_IN_US * 1000 ) put_rotate_wave( .wave( b_i ) );
-    join
-  else
-    fork
-      put_rotate_wave( .wave( b_i ) );
-      #( 3 * EXPECTED_DELAY_IN_US * 1000 ) put_rotate_wave( .wave( a_i ) );
-    join
-endtask
-
-integer i;
-
-task automatic put_rotate_wave(
-  output reg wave
-);
+task put_rotate_wave_a();  
+  integer i;
   begin
     for ( i = 0; i < CLOCK_FREQ_MHZ * EXPECTED_DELAY_IN_US; i = i + 1 )
       begin
         @( posedge clk );
-        wave = $urandom % 2;
+        a = $urandom % 2;
       end
-    wave = 0;
+    a = 0;
     #( 5 * EXPECTED_DELAY_IN_US * 1000 );
     for ( i = 0; i < CLOCK_FREQ_MHZ * EXPECTED_DELAY_IN_US; i = i + 1 )
       begin
         @( posedge clk );
-        wave = $urandom % 2;
+        a = $urandom % 2;
       end
-    wave = 1;
+    a = 1;
   end
 endtask
 
-initial
+task put_rotate_wave_b();  
+  integer i;
   begin
-    a_i = 1;
-    b_i = 1;
-    rst = 1;
-    #CLOCK_SEMI_PERIOD_NS;
-    rst = 0;
-    fork
-      clock_start();
+    for ( i = 0; i < CLOCK_FREQ_MHZ * EXPECTED_DELAY_IN_US; i = i + 1 )
       begin
-        rotate_the_shaft( .dir( 1'b1 ) );
-        $finish;
+        @( posedge clk );
+        b = $urandom % 2;
       end
-    join
+    b = 0;
+    #( 5 * EXPECTED_DELAY_IN_US * 1000 );
+    for ( i = 0; i < CLOCK_FREQ_MHZ * EXPECTED_DELAY_IN_US; i = i + 1 )
+      begin
+        @( posedge clk );
+        b = $urandom % 2;
+      end
+    b = 1;
   end
-*/
+endtask
 
 initial
   begin
@@ -208,104 +114,26 @@ initial
       clk = ~clk;
   end
 
-reg a_req_strobe = 0, b_req_strobe = 0;
-
-task make_a_req_strobe();
-  begin
-    @( posedge clk ); 
-    a_req_strobe = 1'b0;
-    @( posedge clk );
-    a_req_strobe = 1'b1;
-    @( posedge clk );
-    a_req_strobe = 1'b0;
-  end
-endtask
-
-task make_b_req_strobe();
-  begin
-    @( posedge clk ); 
-    b_req_strobe = 1'b0;
-    @( posedge clk );
-    b_req_strobe = 1'b1;
-    @( posedge clk );
-    b_req_strobe = 1'b0;
-  end
-endtask
-
-task left();
-  fork
-    make_b_req_strobe();
-    #( 3 * EXPECTED_DELAY_IN_US * 1000 ) make_a_req_strobe();
-  join
-endtask
-
-task right();
-  fork
-    make_a_req_strobe();
-    #( 3 * EXPECTED_DELAY_IN_US * 1000 ) make_b_req_strobe();
-  join
-endtask
-
-integer i, j;
-
-always @( posedge a_req_strobe )
-  begin
-    for ( i = 0; i < CLOCK_FREQ_MHZ * EXPECTED_DELAY_IN_US; i = i + 1 )
-      begin
-        @( posedge clk );
-        a_i = $urandom % 2;
-      end
-    a_i = 0;
-    #( 5 * EXPECTED_DELAY_IN_US * 1000 );
-    for ( i = 0; i < CLOCK_FREQ_MHZ * EXPECTED_DELAY_IN_US; i = i + 1 )
-      begin
-        @( posedge clk );
-        a_i = $urandom % 2;
-      end
-    a_i = 1;
-  end
-  
-always @( posedge b_req_strobe )
-  begin
-    for ( j = 0; j < CLOCK_FREQ_MHZ * EXPECTED_DELAY_IN_US; j = j + 1 )
-      begin
-        @( posedge clk );
-        b_i = $urandom % 2;
-      end
-    b_i = 0;
-    #( 5 * EXPECTED_DELAY_IN_US * 1000 );
-    for ( j = 0; j < CLOCK_FREQ_MHZ * EXPECTED_DELAY_IN_US; j = j + 1 )
-      begin
-        @( posedge clk );
-        b_i = $urandom % 2;
-      end
-    b_i = 1;
-  end
-
 initial
   begin
-    a_i = 1;
-    b_i = 1;
+    a = 1;
+    b = 1;
     rst_n = 0;
     #1000;
     rst_n = 1;
-    //clock_start();
-  end
-  
-initial
-  begin
-    // increase shim value
-    right();
-    #( ( 10 + 2 ) * EXPECTED_DELAY_IN_US * 1000 );
-    // decrease shim value
-    left();
-    #( ( 10 + 2 ) * EXPECTED_DELAY_IN_US * 1000 );
-    right();
-    #( ( 10 + 2 ) * EXPECTED_DELAY_IN_US * 1000 );
-    right();
-    #( ( 10 + 2 ) * EXPECTED_DELAY_IN_US * 1000 );
-    right();
-    #( ( 10 + 2 ) * EXPECTED_DELAY_IN_US * 1000 );    
+    // increase PWM value
+    dir = 1;
+    put_rotate_waves();
+    // decrease PWM value
+    dir = 0;
+    put_rotate_waves();
+    // and increase again
+    dir = 1;
+    put_rotate_waves();
+    dir = 1;
+    put_rotate_waves();
+    dir = 1;
+    put_rotate_waves();
   end  
 
 endmodule
